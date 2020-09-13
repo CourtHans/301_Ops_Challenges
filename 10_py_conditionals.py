bfd695fddb186569af607bc3c6d1b15b6c76b9dd
CourtHans <cornta@yahoo.com>
2020-09-13 02:55:16 -0400

    moving files

---
+++ 10_py_conditionals.py
@@ -0,0 +1,38 @@
+# Script:                       Op Challenge 10
+# Author:                       Courtney Hans
+# Date of latest revision:      9/11/20
+# Purpose:                      Python Conditionals
+
+# Declaration of variables
+a = 43
+b = 1976
+
+# Declaration of functions
+
+# Main
+
+if a==b:
+    print("a and be are equal!")
+elif a!=b:
+    print("a and be are NOT equal")
+
+if a<b:
+    print("a is less than b")
+elif a>b:
+    print("b is greater than a")
+
+
+if a<=b:
+    print("a is either less than OR equal to b")
+elif a>=b:
+    print("a is either greater than OR equal to b")
+
+
+if a>=101:
+    print("a is greater than 100")
+elif a<=1:
+    print("a is 1")
+else:
+    print("a is between 2 and 100!")
+
+# End
